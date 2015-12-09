cbf36ee8681f121a3336d255d331e257d6d1ade7
gvimlan95 <gvimlan95@gmail.com>
2015-12-09 11:36:55 +0800

    File navigation working

--- DirectoryDisplayController.java
+++ DirectoryDisplayController.java
@@ -1,34 +1,23 @@
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.TextAlignment;
-import javafx.stage.Stage;
-import sun.plugin.javascript.navig.Anchor;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
+import java.lang.Object;
 
 public class DirectoryDisplayController {
 
     private List<Image> images = new ArrayList<>();
+    private Deque<String> pastDirectory = new ArrayDeque<String>();
 
     @FXML
     private TilePane tilePaneItems;
@@ -42,6 +31,8 @@ public class DirectoryDisplayController {
 
     public void listFilesAndFilesSubDirectories(String location){
 
+
+        tilePaneItems.getChildren().clear();
         ListFilesUtil listFilesUtil = new ListFilesUtil();
         File[] fileList=listFilesUtil.listFiles(location);
         for (File file : fileList){
@@ -52,16 +43,9 @@ public class DirectoryDisplayController {
             Image img;
 
             if (file.isFile()){
-
-               // ImageView imgView = new ImageView();
-//                Label label = new Label(fileName.replace(".jpg",""));
-               // Label label = new Label(file.getName());
                  img = new Image("file:"+file.getAbsolutePath());
             }else{
-
-              //  Label label = new Label(file.getName());
                 img = new Image("file:/Users/VIMLANG/G52SAD-Coursework2/icons/folder-open-512.png");
-
             }
 
             imgView.setImage(img);
@@ -75,15 +59,20 @@ public class DirectoryDisplayController {
             pane.setPadding(new Insets(20.0));
 
             pane.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-                System.out.println("Tile pressed ");
-                System.out.print("source "+event.toString());
-                //event.consume();
+                System.out.println(file.getAbsolutePath());
+                listFilesAndFilesSubDirectories(file.getAbsolutePath());
+                pastDirectory.push(location);
             });
 
             tilePaneItems.getChildren().add(pane);
         }
+    }
 
-
+    public void goToPreviousDirectory(){
+        if(!pastDirectory.isEmpty()) {
+            String dir = pastDirectory.pop();
+            listFilesAndFilesSubDirectories(dir);
+        }
     }
 
 }
